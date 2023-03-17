0cc25f2a2ba198aa0fcebfc63c8f33fdde45e2c4
vishu118 <114770536+vishu118@users.noreply.github.com>
2023-03-17 13:44:39 +0530

    use  state

--- src/App.js
+++ src/App.js
@@ -2,16 +2,25 @@ import React, {useState} from "react";
 
 
 const App = () => {
+ const [count,setcount] = useState(0)
+  const Increment = () => {
+  setcount(count+1)
+  };
+  const Decrement = () => {
  
-  const changeName = () => {
-  
+  if(count > 0){
+setcount(count-1)
+  }
   };
   return (
     <>
       <div className="continer">
-        <h1>hello</h1>
-        <button className="btn" onClick={changeName}>
-          Click Me
+        <h1>{count}</h1>
+        <button className="btn" onClick={Increment} >
+           Increment
+        </button>
+        <button className="btn" onClick={Decrement}disabled = {count==0}>
+           Decrement
         </button>
       </div>
     </>
