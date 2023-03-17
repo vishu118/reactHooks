52a896280f74c73c8118bb0bd3927c6aaa252380
vishu118 <114770536+vishu118@users.noreply.github.com>
2023-03-17 14:02:31 +0530

    toggle learned

--- src/App.js
+++ src/App.js
@@ -3,15 +3,33 @@ import React, {useState} from "react";
 
 const App = () => {
  const [count,setcount] = useState(0)
+ const [val,setval] = useState("click to change")
+
   const Increment = () => {
   setcount(count+1)
   };
+
   const Decrement = () => {
- 
-  if(count > 0){
+ if(count > 0){
 setcount(count-1)
   }
   };
+
+ 
+  let handleClick = ()=>{
+    setval('Name Changed')
+    let name = val;
+
+    if(name === "click to change"){
+      setval('Name Changed')
+    }
+    else{
+      setval('click to change')
+    }
+  }
+  
+ 
+
   return (
     <>
       <div className="continer">
@@ -22,6 +40,15 @@ setcount(count-1)
         <button className="btn" onClick={Decrement}disabled = {count==0}>
            Decrement
         </button>
+      </div>
+
+
+      <div className="continer">
+        <h1>{val}</h1>
+        <button className="btn" onClick={handleClick}>
+           Click Me
+        </button>
+       
       </div>
     </>
   );
