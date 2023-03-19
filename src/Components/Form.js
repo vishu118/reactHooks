d4fb15085a36f0871d35f677ac70a03158004490
vishu118 <114770536+vishu118@users.noreply.github.com>
2023-03-19 11:46:53 +0530

    netflex small project

--- src/Components/Form.js
+++ src/Components/Form.js
@@ -1,9 +1,24 @@
-import React from 'react'
+import React,{useState} from 'react'
+import "./CSS/Form.css";
 
 const Form = () => {
+    const [Email, setEmail] = useState("")
+    const [Password, setPassword] = useState("")
+
+    const handleSubmit= ()=>{
+
+    }
   return (
     <>
-        
+    <div className='formSection'>
+    <label htmlFor="email">Email </label>
+        <input type="email" placeholder = "Enter Your Email" name='email' id='email' autoComplete='off' value={Email}/><br/>
+     <label htmlFor="password">Password </label>   
+        <input type="password" placeholder = "Enter Your Password" name='password' id='password'  autoComplete='off'  value={Password}/>
+        <button type='submit' className='btn' onClick={handleSubmit}>Submit</button>
+    </div>
+   
+
     </>
   )
 }
--- src/index.js
+++ src/index.js
@@ -1,11 +1,12 @@
 
 import React from 'react'
 import ReactDOM from 'react-dom/client';
-import './index.css';
+import NteflexCorosoul from './Components/Netflex Corosoul/NteflexCorosoul';
+// import './index.css';
 // import App from './App';
 // import Spreadoperator from './Components/Spreadoperator';
-import ArrayTodo from './Components/ArrayTodo';
-import Form from './Components/Form';
+// import ArrayTodo from './Components/ArrayTodo';
+// import Form from './Components/Form';
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
@@ -16,7 +17,9 @@ root.render(
         {/* <App/> */}
         {/* <Spreadoperator/> */}
         {/* <ArrayTodo/> */}
-        <Form/>
+        {/* <Form/> */}
+        <NteflexCorosoul/>
+
     </>
     
   
